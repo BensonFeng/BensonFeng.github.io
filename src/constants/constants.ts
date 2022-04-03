8c1b4eb9f97e466b16d504a7c7f179c8ba35ae55
BensonFeng <bensonfeng@outlook.com>
2022-04-03 15:41:40 -0400

    changes

---
+++ src/constants/constants.ts
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    title: "PROJECT 1",
+    description:
+      "Lorem ipsum dolor sit amet. Sed voluptate neque et adipisci neque est autem excepturi aut assumenda sunt aut doloribus fuga est accusantium adipisci. Aut internos recusandae et molestiae consequatur non velit quaerat cum modi odio vel doloremque quaerat non laborum commodi. Eos laudantium voluptates et iste praesentium non ",
+    image: "/images/1.png",
+    tags: ["tech1", "tech2", "tech3", "tech4"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 0,
+  },
+  {
+    title: "E-PROJECT 2",
+    description:
+      "Lorem ipsum dolor sit amet. Sed voluptate neque et adipisci neque est autem excepturi aut assumenda sunt aut doloribus fuga est accusantium adipisci. Aut internos recusandae et molestiae consequatur non velit quaerat cum modi odio vel doloremque quaerat non laborum commodi. Eos laudantium voluptates et iste praesentium non ",
+    image: "/images/2.png",
+    tags: ["tech1", "tech2", "tech3", "tech4"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 1,
+  },
+  {
+    title: "PROJECT 3",
+    description:
+      "Lorem ipsum dolor sit amet. Sed voluptate neque et adipisci neque est autem excepturi aut assumenda sunt aut doloribus fuga est accusantium adipisci. Aut internos recusandae et molestiae consequatur non velit quaerat cum modi odio vel doloremque quaerat non laborum commodi. Eos laudantium voluptates et iste praesentium non ",
+    image: "/images/3.jpg",
+    tags: ["tech1", "tech2", "tech3", "tech4"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 2,
+  },
+  {
+    title: "PROJECT 4",
+    description:
+      "Lorem ipsum dolor sit amet. Sed voluptate neque et adipisci neque est autem excepturi aut assumenda sunt aut doloribus fuga est accusantium adipisci. Aut internos recusandae et molestiae consequatur non velit quaerat cum modi odio vel doloremque quaerat non laborum commodi. Eos laudantium voluptates et iste praesentium non ",
+    image: "/images/4.jpg",
+    tags: ["tech1", "tech2", "tech3", "tech4"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 1111, text: "Lorem ipsum dolor sit amet." },
+  { year: 1111, text: "Lorem ipsum dolor sit amet." },
+  { year: 1111, text: "Lorem ipsum dolor sit amet." },
+  { year: 1111, text: "Lorem ipsum dolor sit amet." },
+  { year: 1111, text: "Lorem ipsum dolor sit amet." },
+];
